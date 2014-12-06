8acb82a55a79fcdbf7f4c555c9fb7515114392c0
Charlotte Vanroelen <charlili@Charlovin-it.local>
2014-12-06 17:10:06 +0100

    Still fixing faulty merge.

--- dao/FlowchartsDAO.php
+++ dao/FlowchartsDAO.php
@@ -12,7 +12,6 @@ class FlowchartsDAO extends DAO{
 		return $stmt->fetchAll(PDO::FETCH_ASSOC);
 	}
 	
-
 	public function selectById($id){
 		$sql = "SELECT * 
 				FROM `flowcharts`
@@ -21,6 +20,16 @@ class FlowchartsDAO extends DAO{
 		$stmt->bindValue(":id",$id);
 		$stmt->execute();
 		return $stmt->fetch(PDO::FETCH_ASSOC);
+	}
+
+	public function selectByUserId($user_id){
+		$sql = "SELECT * 
+				FROM `flowcharts`
+				WHERE `user_id` = :user_id";
+		$stmt = $this->pdo->prepare($sql);
+		$stmt->bindValue(":user_id",$user_id);
+		$stmt->execute();
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
 	}
 
 	public function delete($id){
@@ -35,14 +44,14 @@ class FlowchartsDAO extends DAO{
 	public function insert($data){
 		$errors = $this->getValidationErrors($data);
 		if(empty($errors)){
-			$sql = "INSERT INTO `flowcharts` (`eig1`, `eig2`, `eig3`, `eig4`, `eig5`) 
-					VALUES (:eig1, :eig2, :eig3, :eig4, :eig5)";
+			$sql = "INSERT INTO `flowcharts` (`user_id`, `group_ids`, `shape_ids`, `line_ids`, `name`) 
+					VALUES (:user_id, :group_ids, :shape_ids, :line_ids, :name)";
 			$stmt = $this->pdo->prepare($sql);
-			$stmt->bindValue(':eig1', $data["eig1"]);
-			$stmt->bindValue(':eig2', $data["eig2"]);
-			$stmt->bindValue(':eig3', $data["eig3"]);
-			$stmt->bindValue(':eig4', $data["eig4"]);
-			$stmt->bindValue(':eig5', $data["eig5"]);
+			$stmt->bindValue(':user_id', $data["user_id"]);
+			$stmt->bindValue(':group_ids', $data["group_ids"]);
+			$stmt->bindValue(':shape_ids', $data["shape_ids"]);
+			$stmt->bindValue(':line_ids', $data["line_ids"]);
+			$stmt->bindValue(':name', $data["name"]);
 			if($stmt->execute()){
 				$lastInsertId = $this->pdo->lastInsertId();
 				return $this->selectById($lastInsertId);
@@ -53,27 +62,28 @@ class FlowchartsDAO extends DAO{
 
 	public function getValidationErrors($data){
 		$errors = array();
-		if(!isset($data["eig1"])){
-			$errors["eig1"] = "please fill in a eig1";
+		if(!isset($data["user_id"])){
+			$errors["user_id"] = "please fill in a user_id";
 		}
 
-		if(!isset($data["eig2"])){
-			$errors["eig2"] = "please fill in a eig2";
+		if(empty($data["group_ids"])){
+			$errors["group_ids"] = "please fill in a group_ids";
 		}
 
-		if(empty($data["eig3"])){
-			$errors["eig3"] = "please fill in a eig3";
+		if(empty($data["shape_ids"])){
+			$errors["shape_ids"] = "please fill in a shape_ids";
 		}
 
-		if(!isset($data["eig4"])){
-			$errors["eig4"] = "please fill in a eig4";
+		if(empty($data["line_ids"])){
+			$errors["line_ids"] = "please fill in a line_ids";
 		}
 
-		if(!isset($data["eig5"])){
-			$errors["eig5"] = "please fill in a eig5";
+		if(empty($data["name"])){
+			$errors["name"] = "please fill in a name";
 		}
 		return $errors;
 
 	}
 
 }
+ No newline at end of file
